3cf9c6cbb6f8b5ac0cef244add58bee7c18b899c
long230796 <long230796@gmail.com>
2022-05-25 23:51:09 +0700

    add feature get locate

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const uuid = require('uuid');
-
 const User = require('./module.js')
 
 const port = 3000;
@@ -13,10 +12,64 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+
 app.get('/user/read', (req, res) => {
 	const id = req.query.id;
 	const user = new User();
+
 	res.json(user.getUserById(id));
 })
+
+
+
+app.get('/user/search', (req, res) => {
+	const name = req.query.name;
+	const user = new User();
+
+	res.json(user.searchUser(name.toLowerCase()));
+})
+
+
+
+app.get('/locate', (req, res) => {
+	const n = req.query.n;
+	const id = req.query.userId;
+
+	const user = new User();
+	res.json(user.getNearestUser(id, n));
+
+})
+
+
+app.post('/user/add', (req, res) => {
+	const id = uuid.v4();
+	const body = req.body;
+	const user = new User();
+
+	body.id = id;
+	res.json(user.addUser(body));
+
+})
+
+
+
+app.put('/user/edit/:id', (req, res) => {
+	const id = req.params.id;
+	const userData = req.body;
+	const user = new User();
+
+	res.json(user.editUserById(id, userData));
+})
+
+
+
+app.delete('/user/edit/:id', (req, res) => {
+	const id = req.params.id;
+	const user = new User();
+
+	res.json(user.rmUserById(id));
+})
+
+
 
 app.listen(port, () => console.log(`server listening on port ${port}`))
--- module.js
+++ module.js
@@ -8,18 +8,130 @@ class User {
 		this.coordinate = coordinate;
 	}
 
+
 	getUserById(id) {
 		const data = jsonfile.readFileSync('./db.json');
+		const users = data.users;
 
-		let user = data.users.filter((item) => {
-			return item.id === id;
-		})
+		let user = users.filter((item) =>
+			item.id === id
+		)
 		if (user.length > 0) {
 			return user;
 		} else {
-			return null;
+			return {data: null};
 		}
 
+	}
+
+
+	addUser(body) {
+		const data = jsonfile.readFileSync('./db.json');
+		data.users.push(body);
+
+		return this.writeFile(data);
+	}
+
+
+	editUserById(id, userData) {
+		let data = jsonfile.readFileSync('./db.json');
+		let users = data.users;
+
+		for (let i = 0; i < users.length; i ++) {
+			if (users[i].id === id) {
+				for (let key in userData) { users[i][key] = userData[key] }
+				data.users = users;
+				return this.writeFile(data);
+			} else {
+				return {data: null};
+			}
+		}
+	}
+
+
+	rmUserById(id) {
+		let data = jsonfile.readFileSync('./db.json');
+		let users = data.users;
+
+		let newUser = users.filter((obj) =>
+			obj.id !== id
+		)
+
+		data.users = newUser;
+		return this.writeFile(data);	
+
+	}
+
+
+	searchUser(name) {
+		const data = jsonfile.readFileSync('./db.json');
+		const users = data.users;
+
+		let newUser = users.filter((obj) => 
+			(obj.firstname.toLowerCase().includes(name) || obj.lastname.toLowerCase().includes(name))
+		)
+
+		newUser = newUser.sort().reverse();
+		return newUser.length > 0 ? newUser : {data: null}
+
+	}
+
+
+	writeFile(data) {
+		try { jsonfile.writeFileSync('./db.json', data, { spaces: 2 }); }
+		catch(e) { return e; }
+
+		return {success: true};	
+	}
+
+	plusElement(e) {
+		let temp = 0;
+		for (let i = 0; i < e.length; i++) {
+		  temp += parseInt(e.charAt(i));
+		}
+
+		return temp;
+	}
+
+	getCoordinate(user) {
+		const x = this.plusElement(user.coordinate.split(':')[0]);
+		const y = this.plusElement(user.coordinate.split(':')[1]);
+
+		return {x:x, y:y};
+	}
+
+
+	getNearestUser(id, n) {
+		const data = jsonfile.readFileSync('./db.json');
+		const users = data.users;
+		const user = users.find((item) => {
+			return item.id === id;
+		})
+
+		const target = this.getCoordinate(user);
+
+		let distance = users.map((item) => {
+			if (item.id !== user.id) {
+				let userCoordinate = this.getCoordinate(item);
+				let distance = this.getDistance(target, userCoordinate);
+
+				item.distance = distance
+				return item;
+			}
+
+		})
+		// sort ascending
+		distance.sort((a, b) => a.distance - b.distance);
+
+		return {data: distance.slice(0, n)}; 
+
+	}
+
+	getDistance(target, position) {
+		let m = Math.pow((position.x - target.x), 2);
+		let n = Math.pow((position.y - target.y), 2);
+
+		return Math.sqrt( m + n )
 	}
 }
 
